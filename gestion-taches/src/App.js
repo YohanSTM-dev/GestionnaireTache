856e6939e6b7ee25c1f56c8126017a628231cef1
YohanSTM-dev <yohan.saint-marc@hotmail.com>
2025-05-27 14:25:47 +0200

    installation des fichiers et dosiers du projet de gestionnaire

---
+++ gestion-taches/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1> Gestionnaire de tâches</h1>
+      <p> Bienvenue sur ce gestionnaire de tâches</p>  
+    </div>
+  );
+}
+
+export default App;
