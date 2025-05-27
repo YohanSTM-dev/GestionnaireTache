e25ece155f636defe0538951e2519469ab952f2d
YohanSTM-dev <yohan.saint-marc@hotmail.com>
2025-05-27 20:51:07 +0200

    ajout du bouton ajouter et supprimer une tache avec les useState

--- gestion-taches/src/App.js
+++ gestion-taches/src/App.js
@@ -1,12 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {useState} from 'react';
 
 function App() {
+  // 1️⃣ Liste de tâches (données simulées pour commencer)
+  const [taches, setTaches] = useState([]);
+  const [nouvelleTache, setNouvelleTache] = useState('');
+
+  const ajouterTache = () => {
+    if(nouvelleTache.trim() === '') return;
+    const new_tache = { 
+      id: crypto.randomUUID(), // Génère un ID unique
+      text: nouvelleTache.trim() // Enlève les espaces inutiles 
+    }
+    setTaches([...taches,new_tache]);
+    setNouvelleTache('');
+  }
+
+  const supprimerTache = (id) => {
+    const nouvelleTache = taches.filter (tache=> tache.id !== id);
+    setTaches(nouvelleTache);
+
+  }
+
+  
   return (
-    <div >
-      <h1> Gestionnaire de tâches</h1>
-      <p> Bienvenue sur ce gestionnaire de tâches</p>  
+    <div style={{ padding: '20px' }}> 
+      <h1> Gestionnaire de Tâches </h1>
+      <input
+        type="text"
+        value={nouvelleTache}
+        onChange={(e) => setNouvelleTache(e.target.value)}
+        placeholder="Nouvelle tâche"
+      />
+
+      <button onClick={ajouterTache} style ={{marginLeft : '10px'}}> 
+        Ajouter 
+      </button>
+
+      
+      <ul>
+        {taches.map((tache) => (
+          <li key = {tache.id}> 
+          {tache.text}
+          <button onClick = {() => supprimerTache(tache.id)} style={{marginLeft : '10px'}}>
+          Supprimer
+          </button>
+          </li>
+        ))}
+      </ul>
     </div>
+    
   );
 }
 
